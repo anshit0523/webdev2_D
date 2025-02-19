73461086804657c1741548042f52a4d5587b2128
anshit0523 <fernnadosaranajr@gmail.com>
2025-02-20 06:49:25 +0800

    second commit

---
+++ act/resources/views/carsList.blade.php
@@ -0,0 +1,19 @@
+@extends('bases')
+@section('title', 'carsList')
+
+<div>
+    <table class="table">
+        <thead>
+            <tr>
+               
+                <th scope="col">#</th>
+                <th scope="col">Carbrand</th>
+                <th scope="col">Model</th>
+                <th scope="col">Plate Number</th>
+            </tr>
+        </thead>
+       
+    </table>
+
+  
+</div>
